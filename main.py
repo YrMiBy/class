28532174a93c004dc72a85fad6ce060c1359cbc2
Юрий <ymbykov@gmail.com>
2024-05-09 13:46:35 +0300

    add new2

--- main.py
+++ main.py
@@ -4,19 +4,27 @@ class Task(): # Создание класса Task (задача)
         self.term = term
         self.status = status
 
-    def description_task(self): # функция добавление задачи
+    def add_task(self, col): # функция добавление задачи
         print(f'{self.description} добавлена задача')
-        self.description += 1
+        self.description += 'не выполнена'
 
     def completing_task(self): # функция отметки выполнения задачи
         print(f'{self.term} выполнена задача')
-        self.term -= 1
+        self.description = 'выполнена'
+
     def status_task(self): # функция статуса задачи
+        if self.description == 'выполнена':
+            self.status = 'выполнена'
+        else:
+            self.status = 'не выполнена'
         print(f'{self.status} статус задачи')
 
-    def info(self):
-        print(f'добавлена задача - {self.description}')
-        print(f'выполнена задача - {self.term}')
-        print(f'статус задачи - {self.status}')
+    def tasks_not(self, col): # количество не выполненных задач
+        for i in range(self.description):
+            if self.description == 'не выполнена':
+                col +=1
+        print(col)
+
+    
 
 
